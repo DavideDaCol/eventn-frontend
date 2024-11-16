474b3d8fc5e7ff59ec2b639fd622edbd58b33e6b
DavideDaCol <118683352+DavideDaCol@users.noreply.github.com>
2024-11-16 20:35:12 +0100

    add sidebar component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,40 @@
 <template>
   <div class="app">
+    <SideBar></SideBar>
     <router-view />
   </div>
 </template>
+
+<script setup>
+  import SideBar from './components/SideBar.vue'
+</script>
+
+<style>
+  /* global CSS variables*/
+  :root{
+    --dark-main: #151515;
+    --light-main: #d5d5d5;
+    --sidebar-max-w: 300px;
+  }
+  
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Source Sans 3', sans-serif;
+  }
+
+  .app{
+    display: flex;
+    /* targets the router view element */
+    main{
+      /* sets the shrink ratio for every flex column */
+      flex: 1 1 0;
+      padding: 1rem;
+      /*media query for sidebar behavior, 768 is the width of an iPad screen in portrait mode*/
+      @media (max-width: 768px) {
+        padding-left: 6rem;
+      }
+    }
+  }
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,65 @@
+<template>
+    <aside>
+        <div class="icons">
+            <span class="material-symbols-outlined">
+                account_circle
+            </span>
+            <span class="material-symbols-outlined">
+                search
+            </span>
+            <span class="material-symbols-outlined">
+                menu
+            </span>
+            <span class="material-symbols-outlined">
+                star
+            </span>
+            <span class="material-symbols-outlined">
+                group
+            </span>
+        </div>
+        <div class="bottom-icons">
+            <span class="material-symbols-outlined">
+                settings
+            </span>
+        </div>
+    </aside>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    aside {
+        display: flex;
+        flex-direction: column;
+        background-color: var(--dark-main);
+        color: var(--light-main);
+        width: calc(2rem + 32px);
+        min-height: 100vh;
+        padding: 1rem;
+    }
+
+    .icons{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        font-size: 2rem;
+        padding: 0.5rem 0;
+    }
+
+    .material-symbols-outlined{
+        font-size: 2rem;
+        padding-bottom: 3rem;
+    }
+
+    .bottom-icons{
+        margin-top: auto;
+    }
+
+    .bottom-icons > .material-symbols-outlined{
+        padding-bottom: 1.5rem;
+    }
+
+</style>
+
