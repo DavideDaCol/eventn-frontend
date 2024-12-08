a951aefc6fc9f7892d6d1af36b287d9ee4c4f92d
DavideDaCol <118683352+DavideDaCol@users.noreply.github.com>
2024-12-08 18:35:48 +0100

    add login page boilerplate

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Login page test</h1>
+</template>
---
+++ src/views/UserView.vue
@@ -0,0 +1,16 @@
+<template>
+    <LoginPage v-if="isLogged" />
+    <UserInfo v-else />
+</template>
+
+<script setup>
+
+    import LoginPage from '../components/LoginPage.vue'
+    // TODO: change when login is implemented
+    const isLogged = true
+
+</script>
+
+<style scoped>
+
+</style>
