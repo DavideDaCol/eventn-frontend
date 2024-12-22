0b3aad63e7429dd5bd09e67b7308745fec763db5
DavideDaCol <118683352+DavideDaCol@users.noreply.github.com>
2024-12-22 15:51:28 +0100

    add search bar component

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Cerca</h1>
+    </div>
+</template>
+
+<script setup>
+    console.log("yeah i'm like that");
+</script>
+
+<style scoped>
+    div{
+        position: absolute;
+        left: calc(2rem + 32px);
+        z-index: 999;
+        width: 20%;
+        height: 95vh;
+        margin: 1.5rem;
+        border-radius: 20px;
+        background-color: var(--dark-main);
+        color: var(--light-main);
+        text-align: center;
+        padding: 1rem;
+    }
+</style>
