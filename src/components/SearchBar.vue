b4c88b9d8b313c984eb06ff2401d20c9d37bfc00
DavideDaCol <118683352+DavideDaCol@users.noreply.github.com>
2025-02-03 23:57:12 +0100

    add search bar functionality

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,16 +1,41 @@
 <template>
     <div>
         <h1>Cerca</h1>
+        <input type="search" v-model="query">
+        <h3 v-for="result in searchResult" :key="result">
+            {{ result.length < 35 ? result : result.substring(0,35).concat('...') }}
+        </h3>
     </div>
 </template>
 
 <script setup>
-    console.log("yeah i'm like that");
+    import { onMounted, ref, computed } from 'vue';
+    import { globalEvents } from '@/stores/events';
+    let titlesOnly = ref([]);
+    const query = defineModel('query');
+    query.value = "";
+
+    onMounted(() => {
+        const allEvents = globalEvents.value;
+        allEvents.forEach(element => {
+            titlesOnly.value.push(element.eventName);
+        });
+        console.log(titlesOnly);
+    });
+
+    const searchResult = computed(() => {
+        return titlesOnly.value.filter(el => 
+            el.toLowerCase().includes(query.value.toLowerCase())
+        );
+    });
+
+    console.log('balls'.length);
 </script>
 
 <style scoped>
     div{
         position: absolute;
+        overflow: auto;
         left: calc(2rem + 32px);
         z-index: 999;
         width: 20%;
@@ -21,5 +46,23 @@
         color: var(--light-main);
         text-align: center;
         padding: 1rem;
+    }
+
+    h3{
+        margin-bottom: 2rem;
+    }
+
+    input{
+        width: 90%;
+        border: unset;
+        border-bottom: 3px solid var(--dark-sec);
+        background-color: var(--dark-main);
+        color: var(--light-main);
+        font-size: large;
+        margin: 1.5rem;
+    }
+
+    input:focus {
+        outline: none;
     }
 </style>
